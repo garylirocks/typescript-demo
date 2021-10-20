6f792b005ea0c6b084dd6e4ab308f42da4beccd7
Gary Li <gary@proworkflow.com>
2021-10-21 11:54:06 +1300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { gql } from '@apollo/client';
 
+const a: string = 20;
+const a: number = 20;
+
+console.log(a);
+
 const App = () => <div>TypeScript is running</div>;
 
 export default App;
